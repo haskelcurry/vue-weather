9f7ef2f5dbef8febf281c9c6ce688c0da6c3183d
user <user@Air-user.Dlink>
2022-08-01 14:36:58 +0300

    units/language/save

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -1,6 +1,28 @@
-<template>
-  <p></p>
-</template>
+<script>
+export default {
+  data() {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
 
-<style scoped>
+<template>
+  <input v-model="text" placeholder="Default Address">
+  <p>{{ text }}</p>
+  <h1>Units</h1>
+     <input name='recommended-by' value='Metric' type='radio'>Metric
+     <input name='recommended-by' value='Standart' type='radio'>Standart
+     <input name='recommended-by' value='Imperial' type='radio'>Imperial
+<h1 id = 'Language'> Language</h1>
+<select id='dropdown'>
+     <option>UA</option>
+     <option>EN</option>
+     <option>DE</option>
+</select>
+
+<button id="save" type="save">Save</button>
+</template>
+<style>
 </style>
