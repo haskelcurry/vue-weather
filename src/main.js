11680a5cf35a18f651c9e08ad1b96b2cf73a1367
user <user@Air-user.Dlink>
2022-08-01 11:35:31 +0300

    page setting

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 import Weather from './pages/Weather.vue';
 import About from './pages/About.vue';
 import App from './App.vue';
+import Settings from './pages/Settings.vue';
 
 import './styles/main.css';
 
 const routes = [
   { path: '/', component: Weather },
-  { path: '/about', component: About }
+  { path: '/about', component: About },
+  { path: '/settings', component: Settings }
 ];
 
 const router = createRouter({
---
+++ src/pages/Settings.vue
@@ -0,0 +1,6 @@
+<template>
+  <p></p>
+</template>
+
+<style scoped>
+</style>
