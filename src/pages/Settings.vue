e34ef53acf9d10099e215e668aa3da6bfa9aca6f
user <user@Air-user.Dlink>
2022-08-02 21:30:51 +0300

    button back

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -4,25 +4,47 @@ export default {
     return {
       text: ''
     }
+  },
+  methods: {
+    back() {
+      this.$router.push();
+    }
   }
 }
 </script>
 
 <template>
+<button @click="back" class="btn btn-primary">Назад</button>
   <input v-model="text" placeholder="Default Address">
   <p>{{ text }}</p>
-  <h1>Units</h1>
+  <form action= ''>
+  <legend>Units</legend><br>
      <input name='recommended-by' value='Metric' type='radio'>Metric
+     <br/>
      <input name='recommended-by' value='Standart' type='radio'>Standart
+     <br/>
      <input name='recommended-by' value='Imperial' type='radio'>Imperial
-<h1 id = 'Language'> Language</h1>
-<select id='dropdown'>
-     <option>UA</option>
-     <option>EN</option>
-     <option>DE</option>
-</select>
-
+   </form><br>
+   <div><br/>
+    <legend id="Language">Language</legend><br>
+    <select id='dropdown'>
+    <br/>
+      <option>UA</option>
+      <option>EN</option>
+      <option>DE</option>
+    </select>
+  </div><br>
+  <br/>
 <button id="save" type="save">Save</button>
 </template>
+
 <style>
+button {
+  cursor: pointer;
+  border: none;
+  font-size: 14px;
+  font-weight: bold;
+  padding: 10px 15px;
+  margin-left: 100px;
+}
 </style>
