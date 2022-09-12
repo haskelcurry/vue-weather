5e8f988ffee979fc24d9c926fc3ba0c010fff3a8
Markel Tuzynskyi <markel.tuzynskyi@cgm.com>
2022-09-12 14:05:57 +0300

    fix from markel....

--- src/components/__tests__/Header.spec.js
+++ src/components/__tests__/Header.spec.js
@@ -1,11 +1,26 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect } from 'vitest';
 
-import { mount } from '@vue/test-utils'
-import Header from '../Header.vue'
+import { mount } from '@vue/test-utils';
+import Header from '../Header.vue';
 
 describe('Header', () => {
   it('renders properly', () => {
-    const wrapper = mount(Header, { props: { title: 'Test Header' } });
+    const wrapper = mount(Header, {
+      props: {
+        title: 'Test Header'
+      },
+      global: {
+        mocks: {
+          $router: {
+            currentRoute: {
+              value: {
+                name: 'weather'
+              }
+            }
+          }
+        }
+      }
+    });
     expect(wrapper.text()).toContain('Test Header');
-  })
-})
+  });
+});
